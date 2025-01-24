bfc641165b76b1d2f223d9729244abae3a8fcd76
Pablo Saavedra <psaavedra@igalia.com>
2025-01-24 20:58:36 +0100

    Add automation mode and signal handling in main.c
    * Introduced `--automation` CLI flag to enable WebKit automation mode.
    * Added automation-related signal handlers to manage web view creation
      and session lifecycle.
    * Added automation support to WebKit context.
    * Implemented signal handlers for SIGINT and SIGTERM to gracefully
      quit the application.

--- main.c
+++ main.c
@@ -5,10 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define WPE_SIMPLE_LAUNCHER_VERSION_MAJOR 1
+#define WPE_SIMPLE_LAUNCHER_VERSION_MINOR 0
+
 // Global variables
 static WebKitWebView *web_view;
 static gchar *current_uri = NULL;
+static int automation = 0;
 static int maximized = 0;
+
+static gboolean automation_views_limit_reached = FALSE;
 
 // Function to compare two strings after trimming whitespace
 int trimmed_strcmp0(const char *str1, const char *str2)
@@ -106,9 +112,48 @@ static gboolean load_view(gpointer user_data) {
     return TRUE;
 }
 
+static void on_web_view_close(WebKitWebView *web_view, void *user_data)
+{
+    g_object_unref(web_view);
+}
+
+static gboolean on_signal_quit(GMainLoop *loop)
+{
+    g_main_loop_quit(loop);
+    return G_SOURCE_CONTINUE;
+}
+
+static WebKitWebView *on_automation_create_web_view(WebKitAutomationSession *session, void *user_data)
+{
+    if (automation_views_limit_reached)
+        return NULL;
+
+    automation_views_limit_reached = TRUE;
+    return web_view;
+}
+
+static void
+on_automation_will_close(WebKitAutomationSession *session)
+{
+    automation_views_limit_reached = FALSE;
+}
+
+static void
+on_web_context_automation_started(WebKitWebContext *context, WebKitAutomationSession *session, gpointer *view)
+{
+    g_autoptr(WebKitApplicationInfo) info = webkit_application_info_new();
+    webkit_application_info_set_version(info, WPE_SIMPLE_LAUNCHER_VERSION_MAJOR, WPE_SIMPLE_LAUNCHER_VERSION_MINOR, 0);
+    webkit_application_info_set_name(info, "wpe-simple-launcher");
+    webkit_automation_session_set_application_info(session, info);
+
+    g_signal_connect(session, "create-web-view", G_CALLBACK(on_automation_create_web_view), view);
+    g_signal_connect(session, "will-close", G_CALLBACK(on_automation_will_close), NULL);
+}
+
 int main(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"maximized", no_argument, &maximized, 1},
+        {"automation", no_argument, &automation, 1},
         {0, 0, 0, 0}
     };
 
@@ -122,22 +167,31 @@ int main(int argc, char *argv[]) {
     }
 
     if (optind >= argc) {
-        g_printerr("Usage: %s [--maximized] <ctrl_file_path>\n", argv[0]);
+        g_printerr("Usage: %s [--maximized] [--automation] <ctrl_file_path>\n", argv[0]);
         return 1;
     }
 
     const gchar *ctrl_file_path = argv[optind];
 
+    WebKitWebContext *web_context = webkit_web_context_get_default();
+    webkit_web_context_set_automation_allowed(web_context, (automation == 1));
+
     // Create a new WebKitWebView
     g_autoptr(WebKitSettings) settings = webkit_settings_new();
-    g_autoptr(WebKitWebsitePolicies) website_policy = webkit_website_policies_new_with_policies(
-            "autoplay", WEBKIT_AUTOPLAY_ALLOW,
-            NULL);;
+    g_autoptr(WebKitWebsitePolicies) website_policy = webkit_website_policies_new();
     web_view = g_object_new(WEBKIT_TYPE_WEB_VIEW,
-            "settings", settings,
-            "web-context", webkit_web_context_get_default(),
-            "website-policies", website_policy,
-            NULL);
+                            "settings", settings,
+                            "web-context", web_context,
+                            "website-policies", website_policy,
+                            "is-controlled-by-automation", (automation == 1),
+                            NULL);
+    g_signal_connect(web_view, "close", G_CALLBACK(on_web_view_close), NULL);
+
+    if (automation) {
+        g_signal_connect(web_context,
+                         "automation-started",
+                         G_CALLBACK(on_web_context_automation_started), web_view);
+    }
 
     WPEToplevel *toplevel = wpe_view_get_toplevel(webkit_web_view_get_wpe_view(web_view));
     wpe_toplevel_resize(toplevel, 1024, 768);
@@ -152,6 +206,8 @@ int main(int argc, char *argv[]) {
     // Create and run the main loop
     g_autoptr(GMainLoop) loop = g_main_loop_new(NULL, TRUE);
     g_main_loop_run(loop);
+    g_unix_signal_add(SIGINT, G_SOURCE_FUNC(on_signal_quit), loop);
+    g_unix_signal_add(SIGTERM, G_SOURCE_FUNC(on_signal_quit), loop);
 
     webkit_web_view_try_close(web_view);
 
